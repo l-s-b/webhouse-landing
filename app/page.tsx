99ff252981d2bc69ffeac616b666d67cc686bff3
l-s-b <lucio_sb@yahoo.com.ar>
2025-07-13 19:33:04 -0300

    1.1.0 - 1st edit Post-vercel deploy

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "Webhouse",
+  description: "Soluciones web rápidás y eficientes para potenciar tu negocio.",
   // other metadata
 };
 
@@ -24,13 +24,13 @@ export default function Home() {
       <Hero />
       <Productos />
       <Servicios />
-      <Video />
-      <Brands />
-      <AboutSectionOne />
-      <AboutSectionTwo />
-      <Testimonials />
-      <Pricing />
-      <Contact />
+      {/* <Video /> */}
+      {/* <Brands /> */}
+      {/* <AboutSectionOne /> */}
+      {/* <AboutSectionTwo /> */}
+      {/* <Testimonials /> */}
+      {/* <Pricing /> */}
+      {/* <Contact /> */}
     </>
   );
 }
